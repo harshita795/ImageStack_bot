4701a4810cecf8e772d493cb8386634cc7ccee85
harshita795 <yadavharshita261303@gmail.com>
2025-02-23 21:30:26 +0530

    feat: server added

--- index.js
+++ index.js
@@ -113,3 +113,15 @@ bot.on("messageCreate", async (msg) => {
 });
 
 bot.login(token);
+
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Bot is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
